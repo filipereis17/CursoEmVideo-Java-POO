f5a260585a8e810462a8f572c258f140a93c3078
Filipe Reis <52473082+filipereis17@users.noreply.github.com>
2023-05-15 09:39:41 -0400

    Aula 13
    Polimorfismo de Sobrecarga

---
+++ Curso-Java-POO/Aula13/src/aula13/Aula13.java
@@ -0,0 +1,48 @@
+package aula13;
+public class Aula13 {
+    public static void main(String[] args) {
+       //Programa Pruncipal 
+       Cachorro c = new Cachorro();
+       //x.emitirSom();
+       
+       //Frase
+       //c.reagir("Vai apanhar"); 
+       //c.reagir(11, 45);
+       
+       //Hora
+       //c.reagir(19, 00);
+       
+       //Dono?
+       //c.reagir(true);
+       //c.reagir(false);
+       
+       //Idade e Peso
+       c.reagir(2, 12.5f);
+       c.reagir(17, 4.5f);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
